1dfdcff2851762123c693fa6f64535de0fa42dc3
rashi123456789 <rashisharma22111997@gmail.com>
2020-10-08 01:20:48 +0530

    Updates

---
+++ precache-manifest.b9968ff6d9c4f4a9df97301597ea1871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6080c3a799cda548954483ac01f61aa5",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "636176fd804a0cf1cec9",
+    "url": "/movie-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "78a67f62b2b2a38e1fed",
+    "url": "/movie-app/static/css/main.4d8b023a.chunk.css"
+  },
+  {
+    "revision": "636176fd804a0cf1cec9",
+    "url": "/movie-app/static/js/2.3c5917a5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/movie-app/static/js/2.3c5917a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a67f62b2b2a38e1fed",
+    "url": "/movie-app/static/js/main.a01daee1.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
